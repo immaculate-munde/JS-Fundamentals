1ec1a71617277a8e17d6284e64b57b1892bf0bf8
Munde Immaculate <immaculatemunde1@gmail.com>
2025-05-29 02:21:06 +0300

    Add 3 arguements

--- 3-value_arguement.js
+++ 3-value_arguement.js
@@ -1,6 +1,5 @@
-#!/usr/bin/node
-if (process.argv[2] === undefined) {
-  console.log('No argument');
-} else {
-  console.log(process.argv[2]);
-}
+const args = process.argv;
+let i = 2;
+let output = args[i] || 'No argument';
+
+console.log(output);
