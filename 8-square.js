2c470955b429ffede886e9bdf995fbbc4671571f
Munde Immaculate <immaculatemunde1@gmail.com>
2025-05-29 02:03:56 +0300

    Corrected the Nop bit

--- 8-square.js
+++ 8-square.js
@@ -1,5 +1,11 @@
-const size = parseInt(process.argv[2]);
+#!/usr/bin/node
 
-for (let i = 0; i < size; i++) {
-  console.log("X".repeat(size));
+const size = Number(process.argv[2]);
+
+if (isNaN(size)) {
+  console.log('Nop');
+} else {
+  for (let i = 0; i < size; i++) {
+    console.log('X'.repeat(size));
+  }
 }
