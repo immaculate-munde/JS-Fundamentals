7212608777e77ea8cf4a9c9bcd9ab7a4d111ed07
Munde Immaculate <immaculatemunde1@gmail.com>
2025-05-29 02:06:47 +0300

    Corrected the missing bit

--- 8-square.js
+++ 8-square.js
@@ -3,7 +3,7 @@
 const size = Number(process.argv[2]);
 
 if (isNaN(size)) {
-  console.log('Nop');
+  console.log('Missing size');
 } else {
   for (let i = 0; i < size; i++) {
     console.log('X'.repeat(size));
