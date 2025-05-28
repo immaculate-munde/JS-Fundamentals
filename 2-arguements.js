395e1711f38d1c9618ae780b55ec03d9f7ad4a6d
Munde Immaculate <immaculatemunde1@gmail.com>
2025-05-29 00:58:12 +0300

    Updated 2 arguements

--- 2-arguements.js
+++ 2-arguements.js
@@ -1,9 +1,9 @@
-let args = process.argv.slice(2);
+const args = process.argv.slice(2); // Get command-line arguments (excluding 'node' and file name)
 
 if (args.length === 0) {
-  console.log("No argument");
+  console.log('0 argument');
 } else if (args.length === 1) {
-  console.log("Argument found");
+  console.log('1 argument');
 } else {
-  console.log("Arguments found");
+  console.log(`${args.length} arguments`);
 }
